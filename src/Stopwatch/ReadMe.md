b6d3d2a6792e2f1d12d6c7f89e64817371f225f5
Neir Besnoi <49652823+besnoi@users.noreply.github.com>
2022-07-11 20:03:24 +0530

    #1 Added Stopwatch
    Initial commit, future commits unlikely

---
+++ src/Stopwatch/ReadMe.md
@@ -0,0 +1,25 @@
+
+<h1 align='center'> <img width=32 src='https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/stopwatch.png'> Stopwatch</h1>
+<p align='center'>
+    <img src='../../_img/stopwatch.jpg'>
+</p>
+
+## Installation
+
+Install the [requirements](#requirements)
+```bash
+pip install tkextrafont
+pip install sv_ttk
+```
+
+## Download
+
+Click here to [Download Stopwatch](https://downgit.github.io/#/home?url=https://github.com/besnoi/pyapps/tree/master/src/Stopwatch)
+
+## Requirements
+- tkextrafont
+- sv_ttk
+
+## License
+
+See [LICENSE.MD](../../LICENSE.MD) and also check the license for packages if you are serious about commercial app deployment
--- src/Stopwatch/main.py
+++ src/Stopwatch/main.py
@@ -15,7 +15,46 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkextrafont import Font
+from datetime import datetime
 import sv_ttk
+
+## Global variables
+
+counter  = 66600   # Beginning of Time 
+runnning = False   # Stopwatch running?
+
+## Define Core Logic
+
+def start():
+    global running
+    running = True
+    def count():
+        if not running:
+            return
+        global counter
+        dt=datetime.fromtimestamp(counter)
+        label['text']=dt.strftime('%M:%S:%f')[:-4]
+        label.after(10,count)
+        counter += 0.01
+    startBtn['state']='disabled'
+    restartBtn['state']='normal'
+    count()
+    return 'break'
+
+def stop():
+    global running
+    running = False
+    startBtn['state']='normal'
+    restartBtn['state']='normal'
+    return 'break'
+
+def restart():
+    global counter
+    counter  = 66600
+    restartBtn['state']='disabled'
+    label['text']='00:00:00'
+    return 'break'
+
 
 ## Init Window
 win = Tk()
@@ -23,21 +62,24 @@ win.title("Stopwatch")
 win.attributes("-alpha", 0.90)
 win.resizable(False, False)
 
+## Init Digital Font
+font = Font(file="digital-7.mono.ttf",family='Digital-7 Mono')
+assert font.is_font_available("Digital-7 Mono")
+assert "Digital-7 Mono" in font.loaded_fonts()
+
 ## Init theme
 sv_ttk.set_theme("dark")
 
-## Init Digital Font
-font = Font(file="digital-7.ttf",family='Digital-7')
-assert font.is_font_available("Digital-7")
-assert "Digital-7" in font.loaded_fonts()
-
 ### Init Layout
-text = Label(win,text='00:00:00',font='Digital-7 64')
-text.configure(style='Green.TLabel')
-text.grid(row=0,columnspan=3,padx=10,pady=10)
+label = Label(win,text='00:00:00',font=('Digital-7 Mono', 80))
+label.configure(style='Green.TLabel')
+label.grid(row=0,columnspan=3,padx=50,pady=10,sticky='nwe')
 
-Button(win,text='Start',style='Accent.TButton').grid(row=2,column=0,sticky='we',padx=5)
-Button(win,text='Stop').grid(row=2,column=1,sticky='we',padx=5)
-Button(win,text='Restart').grid(row=2,column=2,sticky='we',padx=5,pady=10)
+startBtn   = Button(win,text='Start',command=start,style='Accent.TButton')
+startBtn.grid(row=2,column=0,sticky='we',padx=5)
+stopBtn    = Button(win,text='Stop',command=stop)
+stopBtn.grid(row=2,column=1,sticky='we',padx=5)
+restartBtn = Button(win,text='Restart',command=restart)
+restartBtn.grid(row=2,column=2,sticky='we',padx=5,pady=10)
 
 win.mainloop()
