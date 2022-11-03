ecf12ac5fcfb877d3c3750b03f6573717ed9da73
Neir Besnoi <49652823+besnoi@users.noreply.github.com>
2022-11-03 17:20:56 +0530

    Update choropleth.py

--- src/Census App/choropleth.py
+++ src/Census App/choropleth.py
@@ -9,14 +9,13 @@ from qtmodern.styles import dark
 from qtmodern.windows import ModernWindow
 
 census = Census()
-STATES = census.getRegionList()
 GENDER = {'males':'#0097fa','females':'#ff659f'}
 RELIGION = {'Hindu':'#ff4500','Muslim':'#009000','Christian':'#1E90FF','Sikh':'#964B00','Buddhist':'#BE7F9D','Jain':'#fa8072','Other':'#A020F0','Unstated':'#808080'}
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("NHFS App")
+        self.setWindowTitle("Census App")
         self.setWindowIcon(QIcon('icon.png'))
         self.layout = QGridLayout(self)
         self.states=QComboBox(self)
@@ -24,11 +23,11 @@ class MainWindow(QWidget):
         self.category=QComboBox(self)
         self.indicator=QComboBox(self)
         self.browser = QWebEngineView()
-        self.states.addItem('India')
-        self.year.addItems(['1991','2001','2011'])
-        self.year.setCurrentText('2011')
+        # self.states.addItem('India')
         self.category.addItems(['Population By Religion','Population By Gender'])
-        for i in STATES:
+        states = census.getRegionList()
+        self.states.addItem("India") # We need India to be on first always
+        for i in states:
             self.states.addItem(i)
         self.layout.addWidget(QLabel('Select State',self),0,0,1,1)
         self.layout.addWidget(self.states,0,1,1,1)
@@ -42,11 +41,34 @@ class MainWindow(QWidget):
         self.layout.addWidget(QSizeGrip(self), 3,3,1,1, Qt.AlignBottom | Qt.AlignRight)
         self.setLayout(self.layout)
 
-        self.states.activated.connect(self.onIndicatorSelect)
-        self.year.activated.connect(self.onIndicatorSelect) # TODO
+        self.states.activated.connect(self.onStateSelect)
+        self.year.activated.connect(self.onYearSelect) # TODO
         self.category.activated.connect(self.onCategorySelect)
         self.indicator.activated.connect(self.onIndicatorSelect)
+        self.onStateSelect()
+
+    def onStateSelect(self):
+        years = census.getYearList(self.states.currentText(),'')
+        prev = self.year.currentText() or str(years[-1])
+        self.year.clear()
+        for i in years:
+            print(i)
+            self.year.addItem(str(i))
+        self.year.setCurrentText(prev) # restore previous value
         self.onCategorySelect()
+
+    def onYearSelect(self):
+        year = int(self.year.currentText())
+        prev = self.states.currentText()
+        states = census.getRegionList(year)
+        self.states.clear()
+        self.states.addItem("India") # We need India to be on first always
+        for i in states:
+            print(i)
+            self.states.addItem(i)
+        self.states.setCurrentText(prev)
+        self.onCategorySelect()
+
 
     def onCategorySelect(self):
         self.indicator.clear()
@@ -88,17 +110,29 @@ class MainWindow(QWidget):
             religion = 'total'
 
         data = census.getChoropleth(year,state,religion,sex)
-        if state=='India' and int(year)<2018: # for 2021 census we will use latest 2019 map
-            mapURL = 'https://besnoi.github.io/maps/json/india2013.json'
+        if state=='India':
+            if int(year)<2000: # for 2021 census we will use latest 2019 map
+                mapURL = 'https://besnoi.github.io/maps/json/india-1999.json'
+            else:
+                mapURL = 'https://besnoi.github.io/maps/json/india-2013.json'
+        elif state=='Andhra Pradesh' and int(year)<2014: # andhra pradesh was bifurcated on 2014
+            mapURL = 'https://besnoi.github.io/maps/json/andhra-pradesh-2014.json'
         else:
             mapURL = 'https://besnoi.github.io/maps/json/'+state.lower()+'.json'
+
+        if int(year)<2000 and state in ['Bihar','Madhya Pradesh','Uttar Pradesh']:
+            mapURL = f'https://besnoi.github.io/maps/json/{state.lower()}-1999.json'
+        elif int(year)<2011 and state in ['Bihar','Madhya Pradesh']:
+            mapURL = f'https://besnoi.github.io/maps/json/{state.lower()}-2001.json'
+
+        print(data)
             
 
         number = 'Percentage' # i assume all data will be in percentage but subject to change?
 
         html = ("<style>body{overflow-y:hidden}#container{height: 500px; min-width: 310px; max-width: 800px; margin: 0 auto;}.loading{margin-top: 10em; text-align: center; color: gray;}</style><script src='https://code.highcharts.com/maps/highmaps.js'></script><div id='container'></div><script>(async ()=>{const topology=await fetch( '"+mapURL+"' ).then(response=> response.json()); const data="+
         str(data)+
-        "; Highcharts.mapChart('container',{chart:{map: topology}, title:{text: '"+indicator[:-4]+" in "+state+" (%)'}, subtitle:{text: '"+state+": "+str(data[0][1])+"',align: 'center',verticalAlign: 'bottom'}, legend:{layout: 'vertical', align: 'right', verticalAlign: 'middle'},mapNavigation:{enabled: true, buttonOptions:{verticalAlign: 'bottom'}}, colorAxis:{align: 'right',verticalMiddle:'middle',min: 0, maxColor: '"+maxColor+"'}, credits:{enabled:false}, series: [{data: data, name: '"+number+"', states:{hover:{color: '#BADA55'}}, dataLabels:{enabled: true, format: '{point.name}'}}]});})();</script>")
+        "; Highcharts.mapChart('container',{chart:{map: topology}, title:{text: '"+indicator[:-4]+" in "+state+" (%)'}, subtitle:{text: '"+state+(' (excluding Jammu and Kashmir)' if year=='1991' and state=='India' else '')+ ": "+str(data[0][1])+"',align: 'center',verticalAlign: 'bottom'}, legend:{layout: 'vertical', align: 'right', verticalAlign: 'middle'},mapNavigation:{enabled: true, buttonOptions:{verticalAlign: 'bottom'}}, colorAxis:{align: 'right',verticalMiddle:'middle',min: 0, minColor: '#ffffff', maxColor: '"+maxColor+"'}, credits:{enabled:false}, series: [{data: data, name: '"+number+"', states:{hover:{color: '#BADA55'}}, dataLabels:{enabled: true, format: '{point.name}'}}]});})();</script>")
         # print(html)
         self.browser.setHtml(html)
         print(data[0][1])
